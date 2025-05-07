cdb6868a382119a8675c70b6cf7b0448a976cd67
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-05-07 18:02:03 +0530

    Testimonial Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero.jsx";
 import FeaturedDestination from "../components/FeaturedDestination.jsx";
 import ExclusiveOffers from "../components/ExclusiveOffers";
+import Testimonial from "../components/Testimonial.jsx";
 // import { useLocation } from "react-router-dom";
 const Home = () => {
   return (
@@ -9,6 +10,9 @@ const Home = () => {
       <Hero />
       <FeaturedDestination />
       <ExclusiveOffers />
+      <Testimonial />
+      {/* <Newsletter /> */}
+      {/* <Footer /> */}
     </>
   );
 };
