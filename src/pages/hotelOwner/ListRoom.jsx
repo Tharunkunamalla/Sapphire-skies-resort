e53329a426ad349101e5b29955073000c9e14a45
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-05-10 01:04:53 +0530

    Added till Add Rooms

--- src/pages/hotelOwner/ListRoom.jsx
+++ src/pages/hotelOwner/ListRoom.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ListRoom = () => {
-  return <div>/* Your component content */</div>;
+  return <div>/* Your component content for commits*/</div>;
 };
 
 export default ListRoom;
